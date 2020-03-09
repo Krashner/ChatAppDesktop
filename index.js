e13028ffcd40806936e22cbb45cf2ad8be5ef5a3
Krashner <jbuttrey@oddrealms.com>
2020-03-08 19:10:19 -0500

    removed menubar and delayed show until loaded

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function createWindow() {
     let win = new BrowserWindow({
         width: 800,
         height: 600,
+        show: false,
         webPreferences: {
             nodeIntegration: false
         }
@@ -13,6 +14,14 @@ function createWindow() {
     // and load the index of the app.
     //to do, use actual address
     win.loadURL('http://localhost:3000')
+
+    //remove the menu bar
+    win.removeMenu()
+    
+    //display window when it's finished loading
+    win.once('ready-to-show', () => {
+        win.show()
+    })
 }
 
 app.whenReady().then(createWindow)
